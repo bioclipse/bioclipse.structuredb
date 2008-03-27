ccc0a8c0624cd436f3921091637418de2ed3df8e
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-27 17:12:37 +0000

    Introduced net.bioclipse.hsqldb into bc2
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5236 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.hsqldb/src/net/bioclipse/hsqldb/HsqldbUtil.java
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     
+ *******************************************************************************/
+
+package net.bioclipse.hsqldb;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.bioclipse.preferences.IPreferenceConstants;
+
+import org.eclipse.ui.PlatformUI;
+import org.hsqldb.Server;
+
+public class HsqldbUtil {
+
+	private static Server server;
+	private final static HsqldbUtil instance = new HsqldbUtil();
+	private int nextFreePos = 1;
+	
+	private Map<Integer, String> paths = new HashMap<Integer, String>();
+	private Map<Integer, String> names = new HashMap<Integer, String>();
+	
+	private HsqldbUtil() {
+		server = new Server();
+	}
+	
+	public static HsqldbUtil getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Starts the Hsqldb server with one local database
+	 */
+	public void startHsqldbServer() {
+
+		final String database = 
+			PlatformUI.getPreferenceStore().getString(IPreferenceConstants.FILEVIEWER_HOME_PATH) 
+			+ File.separator + ".database";
+
+		try {
+			server.checkRunning(false);
+		}
+		catch(RuntimeException e) {
+			return; //Server is already running
+		}
+		server.setDatabaseName(0, "local");
+        server.setDatabasePath(0, database);
+        server.setLogWriter(null);
+        server.setErrWriter(null);
+        server.start();
+	}
+	
+	/**
+	 * Stops the Hsqldb-server
+	 * 
+	 * @throws ClassNotFoundException
+	 */
+	public void stopHsqldbServer() {
+
+		try {
+			Class.forName("org.hsqldb.jdbcDriver");
+			String url = "jdbc:hsqldb:hsql://127.0.0.1";
+			Connection con;
+			
+			con = DriverManager.getConnection(url);
+		
+			String sql = "SHUTDOWN";
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(sql);
+			stmt.close();
+		} 
+		catch (SQLException e) {
+			throw new IllegalArgumentException("Could not perform shutdown statement", e);
+		} 
+		catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Could not find the jdbcDriver class", e);
+		}
+		// no need to close a dead connection!
+		Activator.getDefault().setHsqldbServer(null);
+	}
+	
+	public void addDatabase(String path, String name) {
+		server.stop();
+		try {
+			while(true) {
+				server.checkRunning(true);
+				Thread.sleep(100);
+			}
+		}
+		catch (RuntimeException e) {
+			//now the server is dead
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		names.put(nextFreePos,   name);
+		paths.put(nextFreePos++, path);
+		
+		server = new Server();
+		server.setLogWriter(null);
+        server.setErrWriter(null);
+		for( int key : names.keySet() ) {
+			server.setDatabasePath( key, paths.get(key) );
+			server.setDatabaseName( key, names.get(key) );
+		}
+
+		server.start();
+	}
+}
