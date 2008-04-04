0d7a710634df7161ce7e5c081d5aec75caefaf90
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-04-04 09:03:58 +0000

    Started work on structuredbInstancemanagers
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5348 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/privatebusiness/StructuredbInstanceManager.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     
+ *******************************************************************************/
+package net.bioclipse.structuredb.privatebusiness;
+
+import java.util.List;
+
+import net.bioclipse.structuredb.StructuredbDataSource;
+import net.bioclipse.structuredb.domain.Library;
+
+public class StructuredbInstanceManager 
+	   extends AbstractStructuredbInstanceManager 
+	   implements IStructuredbInstanceManager {
+
+	public List<Library> getAllLibraries(StructuredbDataSource database) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
