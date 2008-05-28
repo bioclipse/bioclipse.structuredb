7253b814ddeb172dddbc81231e68c5a88207d2ab
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-05-28 14:10:13 +0000

    Moved the bitwiseAdd code from the structuredb plugin to the hsqldb plugin. Eclipse dependencies can be a bit troublesome. I really wanted that code in structredb but circular dependencies isn't the way to go... Anyway now substructreSearch can be performed from th JS-console again
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5734 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.hsqldb/src/net/bioclipse/hsqldb/HsqldbHelper.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     
+ *******************************************************************************/
+package net.bioclipse.hsqldb;
+
+
+/**
+ * @author jonalv
+ *
+ */
+public class HsqldbHelper {
+
+    public static byte[] bitAnd(byte[] a, byte[] b) {
+        byte[] result = new byte[a.length];
+        for ( int i = 0; i < result.length; i++ ) {
+            result[i] = (byte) (a[i]&b[i]);
+        }
+        return result;
+    }
+}
