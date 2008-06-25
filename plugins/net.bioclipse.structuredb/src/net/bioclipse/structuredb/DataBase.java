615f759f11f397db8626334681bc6b15c6969b2a
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-25 16:00:25 +0000

    [Structuredb] labels are now shown in the service view (and updated when created/removed)
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5942 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/DataBase.java
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/DataBase.java
@@ -13,11 +13,14 @@ package net.bioclipse.structuredb;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.bioclipse.services.views.model.AbstractServiceContainer;
 import net.bioclipse.services.views.model.AbstractServiceObject;
 import net.bioclipse.services.views.model.IDatabase;
 import net.bioclipse.services.views.model.IServiceContainer;
 import net.bioclipse.services.views.model.IServiceObject;
+import net.bioclipse.structuredb.business.IDatabaseListener;
 import net.bioclipse.structuredb.business.IStructuredbManager;
+import net.bioclipse.structuredb.business.IDatabaseListener.DatabaseUpdateType;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IMenuManager;
@@ -27,15 +30,50 @@ import org.springframework.context.ApplicationContext;
  * @author jonalv
  *
  */
-public class DataBase extends AbstractServiceObject 
-                      implements IDatabase {
+public class DataBase extends AbstractServiceContainer 
+                      implements IDatabase, IDatabaseListener {
 
     public DataBase( String name ) {
         setName( name );
+        Activator.getDefault().getStructuredbManager().addListener(this);
     }
     
     public void drop( Object data ) {
 
         
+    }
+
+    @Override
+    public void createChildren() {
+
+        // remove all children from listening 
+        // (otherwise the garbage collector won't have a chance...)
+        if ( children != null ) {
+            for ( IServiceObject db : children ) {
+                if ( db instanceof IDatabaseListener )
+                Activator.getDefault()
+                         .getStructuredbManager()
+                         .removeListener( (IDatabaseListener)db );
+            }
+        }
+        
+        List<IDatabase> children 
+            = new ArrayList<IDatabase>();
+        
+        for ( net.bioclipse.structuredb.domain.Label l : 
+                         Activator.getDefault()
+                                  .getStructuredbManager()
+                                  .allLabels( getName() ) ) {
+            children.add( new Label( l.getName() ) );
+        }
+        setChildren( children );
+    }
+
+    public void onDataBaseUpdate( DatabaseUpdateType updateType ) {
+
+        if ( updateType == DatabaseUpdateType.LABELS_CHANGED ) {
+            createChildren();
+            fireChanged( this );
+        }
     }
 }
---
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/Label.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     
+ *******************************************************************************/
+package net.bioclipse.structuredb;
+
+import net.bioclipse.services.views.model.AbstractServiceObject;
+import net.bioclipse.services.views.model.IDatabase;
+
+
+/**
+ * @author jonalv
+ */
+public class Label extends AbstractServiceObject 
+                   implements IDatabase {
+
+    public Label(String name) {
+        setName( name );
+    }
+
+    public void drop( Object data ) {
+
+        // TODO Auto-generated method stub
+        
+    }
+}
