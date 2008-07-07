bf31435daed93747996b23980919f266ef929e61
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-07-07 14:56:53 +0000

    Added remove database to the gui
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6115 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/actions/RemoveDatabaseAction.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     
+ *******************************************************************************/
+package net.bioclipse.structuredb.actions;
+
+import java.util.Iterator;
+
+import net.bioclipse.structuredb.Activator;
+import net.bioclipse.structuredb.DataBase;
+import net.bioclipse.structuredb.business.IStructuredbManager;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.actions.ActionDelegate;
+
+
+public class RemoveDatabaseAction extends ActionDelegate {
+
+    private ISelection selection;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void run(IAction action) {
+        
+        IStructuredbManager manager = Activator
+                                      .getDefault()
+                                      .getStructuredbManager();
+        
+        if ( selection instanceof IStructuredSelection ) {
+            IStructuredSelection ss = (IStructuredSelection) selection;
+            System.out.println("Selected: " + ss.getFirstElement() );
+            Iterator i = ss.iterator();
+            while ( i.hasNext() ) {
+                Object o = i.next();
+                if (o instanceof DataBase) {
+                    manager.removeDatabase( ((DataBase)o).getName() );
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void selectionChanged( IAction action, ISelection selection ) {
+        this.selection = selection;
+        super.selectionChanged( action, selection );
+    }
+}
