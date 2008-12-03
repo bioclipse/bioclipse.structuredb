c8070833aa1d8f6ee24d300c11daa9542d405f4c
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-03 13:21:58 +0000

    [structuredb] perfected layout for the create new database dialog.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7932 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/dialogs/CreateNewStructureDBDialog.java
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/dialogs/CreateNewStructureDBDialog.java
@@ -49,15 +49,17 @@ public class CreateNewStructureDBDialog extends TitleAreaDialog {
 
 		final Label databaseNameLabel = new Label(container, SWT.NONE);
 		final FormData fd_databaseNameLabel = new FormData();
-		fd_databaseNameLabel.bottom = new FormAttachment(100, -5);
+		fd_databaseNameLabel.top = new FormAttachment(0, 13);
+		fd_databaseNameLabel.bottom = new FormAttachment(100, -9);
 		fd_databaseNameLabel.left = new FormAttachment(0, 5);
 		databaseNameLabel.setLayoutData(fd_databaseNameLabel);
 		databaseNameLabel.setText("Database name:");
 
 		text = new Text(container, SWT.BORDER);
 		final FormData fd_text = new FormData();
+		fd_text.bottom = new FormAttachment(0, 34);
+		fd_text.top = new FormAttachment(0, 10);
 		fd_text.right = new FormAttachment(100, -10);
-		fd_text.bottom = new FormAttachment(databaseNameLabel, 0, SWT.BOTTOM);
 		fd_text.left = new FormAttachment(databaseNameLabel, 0, SWT.RIGHT);
 		text.setLayoutData(fd_text);
 		setTitle("Create new StructureDB instance");
