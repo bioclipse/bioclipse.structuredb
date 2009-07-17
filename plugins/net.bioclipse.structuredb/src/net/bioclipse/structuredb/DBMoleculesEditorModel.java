483b4292384ffbda80b081145bf27f795e8eb7f9
jonalv <jonathan.alvarsson@gmail.com>
2009-07-17 13:56:56 +0200

    Changed copyright header for all files in structuredb plugin

--- plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/DBMoleculesEditorModel.java
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/DBMoleculesEditorModel.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2007 Bioclipse Project
+ * Copyright (c) 2007 - 2009  Jonathan Alvarsson <jonalv@users.sourceforge.net>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * <http://www.eclipse.org/legal/epl-v10.html>
  *
- * Contributors:
- *      Jonathan Alvarsson
- *     
- *******************************************************************************/
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 package net.bioclipse.structuredb;
 
 import net.bioclipse.cdk.domain.ICDKMolecule;
--- plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/business/StructuredbManagerFactory.java
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/business/StructuredbManagerFactory.java
@@ -1,13 +1,12 @@
- /*******************************************************************************
- * Copyright (c) 2007-2008 The Bioclipse Project and others.
+/*******************************************************************************
+ * Copyright (c) 2007 - 2009  Jonathan Alvarsson <jonalv@users.sourceforge.net>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
- * 
- * Contributors:
- *     Jonathan Alvarsson
- *     
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
  ******************************************************************************/
 
 package net.bioclipse.structuredb.business;
