df92448c6f03e6995b9f2ab637d279284ab3d30f
Arvid Berg <goglepox@users.sourceforge.net>
2009-12-11 16:10:42 +0100

    Double-click opens MoleuclesEditor for labels
    Solves bug #1642
    
    Signed-off-by: jonalv <jonathan.alvarsson@gmail.com>

---
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/actions/OpenActionProvider.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.structuredb.actions;
+
+import net.bioclipse.structuredb.Label;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.navigator.ICommonActionConstants;
+import org.eclipse.ui.navigator.ICommonActionExtensionSite;
+import org.eclipse.ui.navigator.ICommonMenuConstants;
+
+
+/**
+ * @author Arvid
+ *
+ */
+public class OpenActionProvider extends CommonActionProvider {
+
+    OpenLabelAction doubleClickAction;
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.navigator.CommonActionProvider#init(org.eclipse.ui.navigator.ICommonActionExtensionSite)
+     */
+    @Override
+    public void init( ICommonActionExtensionSite aSite ) {
+        super.init( aSite );
+        
+        doubleClickAction = new OpenLabelAction("Open");
+        aSite.getStructuredViewer().addSelectionChangedListener(doubleClickAction);
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.actions.ActionGroup#fillActionBars(org.eclipse.ui.IActionBars)
+     */
+    @Override
+    public void fillActionBars( IActionBars actionBars ) {
+
+        super.fillActionBars( actionBars );
+        IStructuredSelection selection = (IStructuredSelection) getContext()
+                        .getSelection();
+        if ( selection.size() == 1 && selection.getFirstElement() instanceof Label ) {
+            doubleClickAction.selectionChanged( selection );
+            actionBars.setGlobalActionHandler( ICommonActionConstants.OPEN,
+                                               doubleClickAction );
+
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.actions.ActionGroup#fillContextMenu(org.eclipse.jface.action.IMenuManager)
+     */
+    @Override
+    public void fillContextMenu( IMenuManager aMenu ) {
+        super.fillContextMenu( aMenu );
+        IStructuredSelection selection = (IStructuredSelection) getContext()
+        .getSelection();
+
+        doubleClickAction.selectionChanged(selection);
+        if (doubleClickAction.isEnabled()) {
+            aMenu.insertAfter(ICommonMenuConstants.GROUP_OPEN, doubleClickAction);
+        }
+    }
+}
---
+++ plugins/net.bioclipse.structuredb/src/net/bioclipse/structuredb/actions/OpenLabelAction.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2007 - 2009  Jonathan Alvarsson <jonalv@users.sourceforge.net>
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.structuredb.actions;
+
+import net.bioclipse.structuredb.Label;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.BaseSelectionListenerAction;
+
+public class OpenLabelAction extends BaseSelectionListenerAction {
+    /**
+     * @param text
+     */
+    protected OpenLabelAction(String text) {
+        super( text );
+    }
+
+    Logger logger = Logger.getLogger(OpenLabelAction.class );
+    
+    @Override
+    public void run() {
+        logger.debug( "Running OpenAnnotationAction" );
+        
+        IStructuredSelection selection = getStructuredSelection();
+
+            IWorkbenchPage wPage = PlatformUI.getWorkbench()
+                                             .getActiveWorkbenchWindow()
+                                             .getActivePage();
+            
+            for ( Object element : 
+                  ( (IStructuredSelection) selection ).toArray() ) {
+                
+                if (element instanceof Label) {
+                    try {
+                        wPage.openEditor( 
+                            (Label) element,
+                            "net.bioclipse.cdk.ui.sdfeditor");
+                    } 
+                    catch ( PartInitException e ) {
+                        logger.debug(
+                            "Faild to open editor for Annotation" );                
+                    }
+                }
+            }
+    }
+}
